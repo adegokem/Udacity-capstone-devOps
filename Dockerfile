0f9932494b94744126955336ed2aa0d01c5d0c29
phan hoai thu <phanhoaithuabc@gmail.com>
2022-05-09 14:51:01 +0700

    fix make lint

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ WORKDIR /app
 
 COPY . /app/
 RUN pip install --upgrade pip && pip install -r requirements.txt
-EXPOSE 8080
+EXPOSE 80
 
 CMD ["python", "run.py"]
--- run.py
+++ run.py
@@ -7,4 +7,4 @@ def index():
     return '<h1>This is the Capstone Project DevOps Udacity, \
             my name is Phan Hoai Thu</h1>'
 
-app.run(host='localhost', port=8080)
+app.run(host='localhost', port=80)
